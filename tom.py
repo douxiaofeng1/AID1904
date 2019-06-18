9fcf895ab4749e76ca534c8e38be375aee8a4cc6
Tedu <729863633@qq.com>
2019-06-18 15:55:43 +0800

    add tom

--- tom.py
+++ tom.py
@@ -1,1 +1,11 @@
-print("Tom")
+def fun1():
+    for i in range(2,101):
+        j=2
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            yield i
+
+for i in fun1():
+    print(i)
